3207c484d70329479f42fa5a244ec2e7d8b2de12
marcellmueller <mlmueller@protonmail.com>
2020-11-17 08:20:10 -0800

    Completed new tweet box, tweet counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $('#tweet-text').bind('input propertychange', function () {
+    let update = 140 - this.value.length;
+    if (update > 0) {
+      $(this).siblings().children('.counter').css('color', 'black');
+      $(this).siblings().children('.counter').text(update);
+    } else {
+      $(this).siblings().children('.counter').css('color', 'red');
+      $(this).siblings().children('.counter').text(update);
+    }
+  });
+});
