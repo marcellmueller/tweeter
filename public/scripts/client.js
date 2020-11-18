3fe049158aee8223825d84cded209b89390e24b4
marcellmueller <mlmueller@protonmail.com>
2020-11-18 14:20:48 -0800

    Implemented error message box, error handling

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -11,19 +11,20 @@ const renderTweets = function (tweets) {
       `<img id="tweet-retweet" src="images/retweet.png"></div>`
     );
     const heart = $(`<img id="tweet-heart" src="images/heart.png"></div>`);
-    const name = $(
-      `<h5 class="tweet-header-username">${tweets[tweet].user.name}</h5>`
+    const name = $(`<h5 class="tweet-header-username"></h5>`).text(
+      tweets[tweet].user.name
     );
-    const avatar = $(
-      `<img class="user-icon" src="${tweets[tweet].user.avatars}"></div>`
+    const avatar = $(`<img class="user-icon" ></div>`).attr(
+      'src',
+      tweets[tweet].user.avatars
     );
-    const handle = $(
-      `<h5 class="tweet-header-email">${tweets[tweet].user.handle}</h5>`
+    const handle = $(`<h5 class="tweet-header-email"></h5>`).text(
+      tweets[tweet].user.handle
     );
-    const content = $(
-      `<div class="article-tweet-content">${tweets[tweet].content.text}</div>`
+    const content = $(`<div class="article-tweet-content"></div>`).text(
+      tweets[tweet].content.text
     );
-    const date = $(`<h6 class="article-tweet-date">${newDate}</h6>`);
+    const date = $(`<h6 class="article-tweet-date"></h6>`).text(newDate);
     $(articleContainer).append(headerContainer, mainContainer, footerContainer);
     $(headerContainer).append(name, handle);
     $(name).prepend(avatar);
@@ -37,17 +38,46 @@ const renderTweets = function (tweets) {
 
 $('.new-tweet-content').on('submit', function (e) {
   e.preventDefault();
-
+  const textInput = $('#tweet-text').val();
   const formData = $('.new-tweet-content').serialize();
+  //Tweet submit error handling
+  if (textInput.length < 1) {
+    errorHandler('Error: This tweet is empty');
+  } else if (textInput.length > 140) {
+    errorHandler('Error: This tweet is over 140 characters');
+  } else if (textInput === null) {
+    errorHandler('Error: Undefined entry');
+  } else if (
+    //check if tweet is good and if so post with AJAX
+    textInput.length >= 1 &&
+    textInput.length <= 140 &&
+    textInput !== null &&
+    textInput !== undefined
+  ) {
+    $('#error-box').slideUp();
+    $.ajax({
+      url: '/tweets/',
+      type: 'POST',
+      data: formData,
+    });
+    $('#tweeter-main').empty();
+    getTweets();
+  } else {
+    errorHandler('Error: Undefined entry');
+  }
+});
 
-  $.ajax({
-    url: '/tweets/',
-    type: 'POST',
-    data: formData,
+const errorHandler = (errorText) => {
+  $('#error-box').text(errorText).slideDown();
+};
+
+const sortTweets = (tweets) => {
+  tweets.sort((a, b) => b.created_at - a.created_at);
+  return tweets;
+};
+const getTweets = () => {
+  $.get('/tweets/', function (data) {
+    renderTweets(sortTweets(data));
   });
-  console.log(formData);
-});
-
-window.onload = $.get('/tweets/', function (data) {
-  renderTweets(data);
-});
+};
+window.onload = getTweets();
