445902c0386e6b307650637bdb723c2c57884a8a
marcellmueller <mlmueller@protonmail.com>
2020-11-17 22:36:02 -0800

    Added AJAX post form for new tweet and GET request to get tweet data and display it on load

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,31 +1,3 @@
-const data = [
-  {
-    user: {
-      name: 'Newton',
-      avatars: 'https://i.imgur.com/73hZDYK.png',
-      handle: '@SirIsaac',
-    },
-    content: {
-      text:
-        'If I have seen further it is by standing on the shoulders of giants',
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: 'Descartes',
-      avatars: 'https://i.imgur.com/nlhLi3I.png',
-      handle: '@rd',
-    },
-    content: {
-      text: 'Je pense , donc je suis',
-    },
-    created_at: 1461113959088,
-  },
-];
-
-const timeSince = (time) => {};
-
 const renderTweets = function (tweets) {
   for (const tweet in tweets) {
     const newDate = moment(tweets[tweet].created_at).fromNow();
@@ -59,9 +31,23 @@ const renderTweets = function (tweets) {
     $(footerContainer).append(date, likesContainer);
     $(likesContainer).append(report, retweet, heart);
     const main = $('#tweeter-main').get(0);
-    console.log(articleContainer);
     $(main).append(articleContainer);
   }
 };
 
-window.onload = renderTweets(data);
+$('.new-tweet-content').on('submit', function (e) {
+  e.preventDefault();
+
+  const formData = $('.new-tweet-content').serialize();
+
+  $.ajax({
+    url: '/tweets/',
+    type: 'POST',
+    data: formData,
+  });
+  console.log(formData);
+});
+
+window.onload = $.get('/tweets/', function (data) {
+  renderTweets(data);
+});
